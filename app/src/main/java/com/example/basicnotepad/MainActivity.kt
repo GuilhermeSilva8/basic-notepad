175b9ffd7d8c0312b037afa3548a84e64c11b854
Guilherme <guilhermecsilva8@gmail.com>
2022-07-04 12:22:09 -0300

    Create TextActivity, enable ViewBinding, put a fab on main layout and set a click listener for moving to TextActivity

--- app/src/main/java/com/example/basicnotepad/MainActivity.kt
+++ app/src/main/java/com/example/basicnotepad/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.basicnotepad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basicnotepad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+
+            val intent = Intent(this, TextActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
