feacc869161a7ad765d111082ebd2affe674b953
Guilherme <guilhermecsilva8@gmail.com>
2022-07-04 12:31:08 -0300

    Add scrolable edit text and a fab to the layout

--- app/src/main/java/com/example/basicnotepad/TextActivity.kt
+++ app/src/main/java/com/example/basicnotepad/TextActivity.kt
@@ -2,10 +2,15 @@ package com.example.basicnotepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basicnotepad.databinding.ActivityTextBinding
 
 class TextActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTextBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_text)
+        binding = ActivityTextBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
